2c8ee2f49ddd802854ff44e000afadeec109dc7d
Marcos Dominguez <mrds0690@gmail.com>
2021-10-08 08:43:08 -0300

    feat: add files and configuration

---
+++ src/app/app.h
@@ -0,0 +1,44 @@
+/*=============================================================================
+ * Author: Marcos Dominguez <mrds0690@gmail.com>
+ * Date: 2021/07/02
+ * Version: 1
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __APP_H__
+#define __APP_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+#include "sapi.h"
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+
+/*=====[Public function-like macros]=========================================*/
+
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __DOMINGUEZ_ACTIVIDAD_1_H__ */
